e897c39a99053be2e3bbadb4e113b02a571230ba
Karmosav <karmo.saviauk@ut.ee>
2023-11-04 11:43:21 +0200

    js update

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,8 @@ document.getElementById("logoButton").addEventListener("click", function() {
     
     var dropdownContent = document.getElementById("dropdown");
     if (dropdownContent.style.display === "block") {
-        console.log("vajutasin")
       dropdownContent.style.display = "none";
     } else {
       dropdownContent.style.display = "block";
-      console.log("vajutasin")
     }
   });
